40e278412380951e825349dd4de94205265802d9
Mpanguhe <41422454+Mpanguhe@users.noreply.github.com>
2024-04-02 19:16:37 -0600

    Add files via upload

---
+++ week16/src/Components/components/Footer.js
@@ -0,0 +1,14 @@
+// components/Footer.js
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer className="bg-dark text-light text-center py-3">
+      <div className="container">
+        &copy; 2022 React CRUD App
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
